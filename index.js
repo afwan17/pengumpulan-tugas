5bdfeaacf737e284feda9cb9a6f8ea0969a79938
Jhon <jhon.riyanto2212@gmail.com>
2025-07-29 16:13:13 +0700

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-..
+// contoh var, let, dan const
+
+var a = 10;
+a = 20;
+
+let b = 30;
+b = 40
+
+var nama = "Jhon";
+let usia = 20;
+
+const hobi = "Berenang"
+
+console.log(a);
+console.log(b);
+console.log(hobi);
+
+
+
+//contoh logika
+
