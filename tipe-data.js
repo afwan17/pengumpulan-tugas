7a4c7a8469b639bbd400240e72280da3d057fb85
Jhon <jhon.riyanto2212@gmail.com>
2025-07-30 16:25:14 +0700

    update tipe-data

---
+++ tipe-data.js
@@ -0,0 +1,60 @@
+//tipe data : primitive
+const bulat = 10
+const nama = "Jhon"
+const alamat = "Banten"
+
+//string
+const double = "Lari"
+const single = "Halo, Selamat Siang"
+const backtick = `Halo saya Jhon`
+const literal = `Selamat siang, saya ${nama} tinggal di ${alamat}`
+
+console.log(literal);
+
+//menggabungkan string
+const kalimat = "Halo" + " " + "Saya" + " " + "Wiwi" + ", " + "saya tinggal di" + " " + "Banten";
+// console.log(kalimat);
+
+//type data Primitive 
+// Booelean
+const benar = true
+const salah = false
+
+//type data Undefined = Variabel yang sudah dideklarasikan tapi belum diberi nilai
+let x ; 
+console.log(x);
+
+//type data Null type data yang merepresentasikan nilai kosong
+const kosong = null
+console.log(kosong);
+
+//type data reference 
+//object
+const orang = {
+    nama : "Jhon",
+    umur : 25,
+    isSingle : true
+}
+//contoh lain
+// const orang = {
+//     nama : "Jhon",
+//     umur : 25,
+//     isSingle : !true
+// }
+
+console.log(orang);
+console.log(orang.isSingle);
+
+//array
+const hewan = ["Macan", "Kumbang", "Kucing"]
+console.log(hewan);
+console.log(hewan[2]); //output : Kucing
+
+//operator
+
+
+
+
+
+
+
